6415763bd2df7fc5c07e86b84833a66d71367742
rileydaly <81534112+rileydaly@users.noreply.github.com>
2021-03-28 15:58:25 -0700

    Fleshed out index functionality

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,13 +1,50 @@
+
+const got = require('got')
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log('JavaScript HTTP trigger function processed a request.');
+  let toReturn 
+  const request_type = req.query.request_type
+  
+  if(request_type === "wind_speed"){
+    const res = await getJSON(req.query)
+    toReturn = res.wind.speed
+  }
+  else if(request_type === "wind_direction"){
+    const res = await getJSON(req.query)
+    toReturn = res.wind.deg
+  }
+  else if(request_type === "wind_gust"){
+    const res = await getJSON(req.query)
+    toReturn = res.wind.gust
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: toReturn || 'no request_type specified.. pass it in as a query param like ?request_type=wind_speed'
+  };
+}
+
+
+async function getJSON({lat='35.10163218697745',lon='-120.63045185072025'}={}) {
+  //gets OpenWeather API
+  //api key = 4616988bca1dbae3e1ae3d0088beed35
+
+  const apiKey = '4616988bca1dbae3e1ae3d0088beed35'
+  try {
+    const response = await got(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+    console.log('res body here:', response.body);
+    const parsed = JSON.parse(response.body)
+    return parsed
+    //=> '<!doctype html> ...'
+  } catch (error) {
+    console.log('err 1212421414 aaa:', error)
+    //=> 'Internal server error ...'
+  }
+
+
+  return 'Yikes it didnt work asdfasdfasdf'
+
 }
---
+++ README.md
@@ -0,0 +1,42 @@
+# WindReader
+
+## Install dependencies
+
+cd ~/Desktop/Sites/WindReaderServerless
+
+npm install
+
+## To Install new dependencies
+
+npm install dependency name
+
+## To run the server locally
+
+hit command-shift-p in vscode
+
+Debug: start debugging 
+
+## To hit local server
+
+copy and paste these urls in chrome
+
+For wind direction:
+http://localhost:7071/api/HttpTrigger1?request_type=wind_direction&lat=55&lon=-120
+
+For wind speed:
+http://localhost:7071/api/HttpTrigger1?request_type=wind_speed&lat=55&lon=-120
+
+
+## To publish to Azure serverless
+
+go to Azure tab on left
+
+Hit blue publish arrow
+
+Click yes
+
+## To hit published function
+
+append specific request after question mark
+
+https://windreader.azurewebsites.net/api/httptrigger1?code=kzfwziTiIZMz3T8qRl3agGzI4MDYebUt3GHifNP6j59QWi3C3a22Ww%3D%3D&request_type=wind_direction&lat=55&lon=-120
